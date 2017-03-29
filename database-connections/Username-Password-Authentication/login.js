ecefa86b7777903ea4e34a568fd0fd3ed5f21647
Michael Willmott <mwillmott@gmail.com>
2017-03-29 14:45:32 +0100

    Don’t rely on local user ID being in the primary Auth0 user ID
    This stores the ID for the user record on the Techbikers server in the app_metadata object rather than relying on the user_id of the primary Auth0 user always containing the Techbikers user ID.
    
    This makes sense because linking accounts if users start to login using other connections could mean that we lose this information.
    
    Signed-off-by: Michael Willmott <mwillmott@gmail.com>

--- database-connections/Username-Password-Authentication/login.js
+++ database-connections/Username-Password-Authentication/login.js
@@ -22,14 +22,20 @@ function login (email, password, callback) {
     }
 
     callback(null, {
-      user_id: body.id.toString(),
+      user_id: "techbikers|" + body.id.toString(),
+      name: body.first_name + " " + body.last_name,
       given_name: body.first_name,
       family_name: body.last_name,
       email: email,
       email_verified: true,
       picture: body.avatar,
-      company: body.company,
-      website: body.website
+      app_metadata: {
+        id: body.id
+      },
+      user_metadata: {
+        company: body.company,
+        website: body.website
+      }
     });
   });
 
