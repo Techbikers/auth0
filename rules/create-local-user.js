425cb907dbfbe58da548ad7c73a3ffb5a5c9d99c
Michael Willmott <mwillmott@gmail.com>
2017-03-30 14:24:50 +0100

    Create user records on the Techbiker server for new users
    This creates an associated user record on the Techbikers server for new users authenticating with Auth0. It checks to see whether there is an id in the app_metadata to determine if it needs to create a user or not.
    
    Signed-off-by: Michael Willmott <mwillmott@gmail.com>

---
+++ rules/create-local-user.js
@@ -0,0 +1,30 @@
+/**
+ * Creates a user record on the Techbikers server when a user authenticates
+ * with Auth0. This should happen during the first login but it will run if
+ * we don't have a link to a local user on the Auth0 user.
+ */
+function (user, context, done) {
+  user.app_metadata = user.app_metadata || {};
+
+  // Check to see if the user already has a local record
+  if (user.app_metadata.id) {
+    return done(null, user, context);
+  }
+
+  // Go ahead and create a new user
+  request.post({
+    url: 'https://techbikers.com/api/riders/',
+    json: Object.assign({}, { email: user.email }, user.user_metadata)
+  }, function(err, response, body) {
+    // Handle errors
+    if (err) {
+      return done(err);
+    }
+
+    // Update the user metadata with the local account id
+    user.app_metadata.id = body.id;
+    auth0.users.updateAppMetadata(user.user_id, user.app_metadata);
+
+    done(null, user, context);
+  });
+}
