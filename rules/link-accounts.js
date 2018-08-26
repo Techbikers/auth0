f7b4f6d7d5b5fcf001afea7b90e9ad043e42f385
Michael Willmott <mwillmott@gmail.com>
2018-08-26 16:08:33 +0100

    Improvements to the link-user rule

--- rules/link-accounts.js
+++ rules/link-accounts.js
@@ -2,13 +2,11 @@
  * Link accounts that share a verified email address
  */
 function (user, context, callback) {
-  var request = require('request@2.56.0');
-  var async = require('async@2.1.2');
-  var userApiUrl = auth0.baseUrl + '/users';
+  var request = require('request');
 
   // Check if email is verified, we shouldn't automatically
   // merge accounts if this is not the case.
-  if (!user.email_verified) {
+  if (!user.email || !user.email_verified) {
     return callback(null, user, context);
   }
 
@@ -16,14 +14,13 @@ function (user, context, callback) {
   // address but not with the current connection (i.e. has this
   // user already got an account with this email address?)
   request({
-    url: userApiUrl,
+    url: `${auth0.baseUrl}/users-by-email`,
     headers: {
-      Authorization: 'Bearer ' + auth0.accessToken
+      Authorization: `Bearer ${auth0.accessToken}`
     },
     qs: {
-      search_engine: 'v2',
-      q: 'email.raw:"' + user.email + '" -user_id:"' + user.user_id + '"'
-   }
+      email: user.email
+    }
   },
   function(err, response, body) {
     // Error handling
@@ -33,56 +30,51 @@ function (user, context, callback) {
     // No errors, handle the response body
     var data = JSON.parse(body);
 
-    // If we have some accounts, link them
-    // (there should only ever be one acount returned if any)
-    if (data.length > 0) {
-      async.each(data, function(targetUser, cb) {
-        var aryTmp = targetUser.user_id.split(/\|(.+)/);
-        var provider = aryTmp[0];
-        var targetUserId = aryTmp[1];
+    // Ignore non-verified users and the current user
+    data = data.filter(u => u.email_verified && (u.user_id !== user.user_id));
 
-        if (targetUser.email_verified) {
-          // Add existing metadata to the user object so that this
-          // gets passed through to any other rules
-          user.app_metadata = targetUser.app_metadata || {};
-          user.user_metadata = targetUser.user_metadata || {};
-
-          // Now update the user with existing metadata
-          auth0.users.updateAppMetadata(user.user_id, user.app_metadata).then(
-            auth0.users.updateUserMetadata(user.user_id, user.user_metadata)
-          ).then(
-            // Finally, link the accounts
-            function() {
-              request.post({
-                url: userApiUrl + '/' + user.user_id + '/identities',
-                headers: {
-                  Authorization: 'Bearer ' + auth0.accessToken
-                },
-                json: {
-                  provider: provider,
-                  user_id: targetUserId
-                }
-              }, function(err, response, body) {
-                if (response.statusCode >= 400) {
-                  cb(new Error('Error linking account: ' + response.statusMessage));
-                } else {
-                  cb(err);
-                }
-              });
-            }
-          ).catch(
-            function(err) {
-              cb(err);
-            }
-          );
-        } else {
-          cb();
-        }
-      }, function(err) {
-        callback(err, user, context);
-      });
-    } else {
-      callback(null, user, context);
+    // We shouldn't be seeing more than one account (as this should always
+    // merge any accounts before the count goes higher than 1)
+    if (data.length > 1) {
+      return callback(new Error('[!] Rule: Multiple user profiles already exist - cannot select base profile to link with'));
     }
+
+    // Skip the rest of the rule if there are no users
+    if (data.length === 0) {
+      console.log('[-] Skipping link rule');
+      return callback(null, user, context);
+    }
+
+    // Get our primary user (who we're going to link this new account to)
+    var primaryUser = data[0];
+
+    // Get the details of our current user
+    var provider = user.identities[0].provider;
+    var userId = user.identities[0].user_id;
+
+    // Bit of logging so we know what's going on
+    console.log(`[+] Running link user rule`);
+    console.log(`    Primary user: ${primaryUser.user_id}`);
+    console.log(`    Secondary user: ${user.user_id}`);
+
+    request.post({
+      url: `${auth0.baseUrl}/users/${primaryUser.user_id}/identities`,
+      headers: {
+        Authorization: `Bearer ${auth0.accessToken}`
+      },
+      json: {
+        provider: provider,
+        user_id: userId
+      }
+    },
+    function(err, response, body) {
+      // Handle any error
+      if (response.statusCode >= 400) {
+        callback(new Error('Error linking account: ' + response.statusMessage));
+      }
+
+      context.primaryUser = primaryUser.user_id;
+      callback(null, user, context);
+    });
   });
 }
